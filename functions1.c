c05ea1ca0c2a416d51c9653a8755c169672b2374
Ethar <etharabdelazeem1@gmail.com>
2023-08-20 13:53:36 +0300

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -11,36 +11,3 @@ int printc(char c)
 {
 	return (write(1, &c, 1));
 }
-
-/**
- * printd - print integers
- * @i: integer
- * Return: nothing
- */
-int printd(int i)
-{
-	int count, return_value = 0, tens = 1, c, num;
-
-	if (i < 0)
-	{
-		printc('-');
-		(return_value)++;
-		i *= -1;
-	}
-	count = no_of_digits(i);
-	c = count - 1;
-	while (c)
-	{
-		tens *= 10;
-		c--;
-	}
-	while (count)
-	{
-		num = (i / tens) % 10;
-		printc('0' + num);
-		(return_value)++;
-		tens /= 10;
-		count--;
-	}
-	return (return_value);
-}
