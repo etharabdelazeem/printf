5a854c9c91c9680dc0a5f739bde3fca8ce953c73
HudaOmer <hudaoe175@gmail.com>
2023-08-21 14:45:52 -0700

    Add %S again

--- printS.c
+++ printS.c
@@ -7,24 +7,15 @@
  */
 int printS(va_list list)
 {
-	int length, return_value;
-	char *string, *string2;
+	int length = 0, return_value;
+	char *string;
 
-	string2 = va_arg(list, char *);
-	if (!string2)
+	string = va_arg(list, char *);
+	if (!string)
 	{
 		write(1, "(null)", 6);
 		return (6);
 	}
-
-	length = _strlen(string2) + 1;
-	string = malloc(sizeof(string2[0]) * length);
-	string = string2;
-
-	if (!string)
-		free(string);
-	length = 0;
-
 	while (string[length] != '\0')
 	{
 		if (string[length] < 32 || string[length] >= 127)
@@ -40,6 +31,5 @@ int printS(va_list list)
 			_putchar(string[length]), (return_value)++;
 		length++;
 	}
-	free(string);
 	return (return_value);
 }
