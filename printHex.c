bd425605d5485a7b4edd1f9fb6c2381f54b73fca
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-08-22 18:50:56 +0200

    Update printHex.c

--- printHex.c
+++ printHex.c
@@ -1,33 +1,33 @@
 #include "main.h"
+
 /**
- * printH - printhex
+ * printH - prints hex
  * @i: number to print
  * Return: printed amount
  */
 int printH(unsigned int i)
 {
-	int return_value = 0, base = 16;
+	int count, return_value = 0, tens = 1, c, num, base = 16;
 	char letter[] = {'A', 'B', 'C', 'D', 'E', 'F'};
-	unsigned int rem;
 
-	rem = i % 16;
-	if (i == 0)
-		return (return_value);
-	if (i == 1)
+	count = no_of_digits(i, base);
+	c = count - 1;
+	while (c)
 	{
-		_putchar(i + '0');
-		return (return_value);
+		tens *= base;
+		c--;
 	}
-
-	printH(i / 16);
-
-	if (rem > 9)
-		_putchar(letter[rem - 10]);
-	else
-		_putchar(rem + '0');
-
-	(return_value) = no_of_digits(i, base);
-
+	while (count)
+	{
+		num = (i / tens) % base;
+		if (num > 9)
+			_putchar(letter[num - 10]);
+		else
+			_putchar('0' + num);
+		(return_value)++;
+		tens /= base;
+		count--;
+	}
 	return (return_value);
 }
 
@@ -42,15 +42,8 @@ int printHEX(va_list list)
 	unsigned int i2;
 
 	i = va_arg(list, int);
-
-	if (i == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-	else
-		i2 = i;
-	(return_value) = printH(i2);
+	i2 = i;
+	(return_value) += printH(i2);
 
 	return (return_value);
 }
